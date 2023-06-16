cc5fb97936497af45dd80036bfdf21f3e2d7a79a
Razak <moabdulrazak17@gmail.com>
2023-06-16 14:00:37 +0000

    chore: connect DB

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"database/sql"
+
 	_ "github.com/lib/pq"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func main() {
+	db := connectDB()
+
+	boil.SetDB(db)
+}
+
+func connectDB() *sql.DB {
+	db, err := sql.Open("postgres", "postgres://postgres:pass@localhost:2345/postgres?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
